e6b5b5ebaaa03d7bbd9ad2374388e5648fd0def0
Sanjay <sanjaykeloth321@gmail.com>
2024-09-09 16:28:37 +0530

    feat(resident): add profile card component in resident home page

--- src/pages/resident/ResidentHomePage.tsx
+++ src/pages/resident/ResidentHomePage.tsx
@@ -1,12 +1,20 @@
 import ResidentNavbar from "../../components/resident/navbar/ResidentNavbar";
 import ResidentHome from "@/components/resident/home/ResidentHome";
+import CommunityProfileCard from "@/components/resident/community/CommunityProfileCard";
 
 const ResidentHomePage = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-100 to-blue-200">
       <ResidentNavbar />
-      <main className="max-w-6xl mx-auto py-6 px-4 sm:px-6 lg:px-8 pt-24">
-        <ResidentHome />
+      <main className="w-full pt-24 flex justify-center">
+        <div className="w-full max-w-7xl flex flex-col lg:flex-row lg:items-start px-4 sm:px-6 lg:px-8">
+          <div className="lg:w-64 xl:w-64 flex-shrink-0 mb-6 lg:mb-0 lg:sticky lg:top-24">
+            <CommunityProfileCard />
+          </div>
+          <div className="flex-grow max-w-2xl w-full mx-auto lg:ml-8">
+            <ResidentHome />
+          </div>
+        </div>
       </main>
     </div>
   );
